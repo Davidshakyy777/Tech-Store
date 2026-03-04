75ebbc47429b63945e65191004deb2d8dfb64259
Davidshakyy777 <Davidshakyy777@gmail.com>
2026-03-04 23:49:55 +0500

    Fix: relative paths in index.html

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/Tech-Store/', // Бұл жол GitHub-та сайт көрінуі үшін ӨТЕ МАҢЫЗДЫ
+  plugins: [react(), tailwindcss(),],
 })
